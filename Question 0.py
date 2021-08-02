581f14ad9bd982384b7c1864063939c75b7b82b0
bjdabc <b_j_d@hotmail.co.uk>
2021-08-02 19:48:09 +0100

    new stuff

--- Question 0.py
+++ Question 0.py
@@ -1,2 +1,6 @@
-help(print)
-dfgjn
+def concise_is_negative(num):
+    return True if bool(num) else False
+
+print(concise_is_negative(0))
+print(concise_is_negative(1))
+print(concise_is_negative(-1))
